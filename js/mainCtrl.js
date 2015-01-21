ab7a2e822e38b9d3bed1fa615de786469d2d862d
Evan Palsson <evanpalsson@gmail.com>
2015-01-21 16:26:56 -0700

    adding todays work

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,22 +1,62 @@
 var app = angular.module('chatroom');
 
 app.controller('mainCtrl', function($scope, parseService){
+
+  $scope.getParseData = function(){   //<-- just a name for the function that gets invoked at the very bottom
+    parseService.getData()            //<-- this invokes the getData function in the service
+      .then(function(data){           //<-- this is the object being taken in
+        $scope.messages = data.data.results;  //<-- this is the object followed by the key and then the property.
+    })
+  };
+
+  $scope.showNewMessage = true; //<-- 
+  $scope.showFilterMessage = true; //<-- don't see any reason to have this.
+
+  $scope.showFilterMessage = function(){
+    // $scope.showNewMessage = false; //<-- don't see any reason to have this.
+    $scope.showFilterMessage = true;
+  }
+
+  // $scope.showNew = function(){         //<-- this doesn't do anything.
+  //   $scope.showNewMessage = true;
+  //   $scope.showFilterMessage = false;
+  // }
+
+
+  $scope.postData = function(){
+    var postMessage = {};                 //<-- this creates a jar that gets filled with an object.
+    postMessage.text = $scope.message;    //<-- the message is coming from the HTML and being passed 
+                                          //into the jar with a property called 'text' being created.
+    parseService.postData(postMessage);   //<-- the jar is passed to the postData function in the service
+    $scope.message = '';
+    parseService.postData($scope.message)
+  };
+  $scope.getParseData();                  //<-- invoking the getParseData function up above so when a message
+                                          //gets sent to the database, it updates on the HTML page
+
+
+
+
+
+
   //In your controller you'll have a getParseData function and a postData function, but should be placed on $scope.
 
-  //The getParseData function will call the getData method on the parseService object. You'll then save the result of that request to 
-  //your controllers $scope as messages ($scope.messages)
+  //The getParseData function will call the getData method on the parseService object. You'll then save the result 
+  //of that request to your controllers $scope as messages ($scope.messages)
 
 
 
-  //The postData function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
-  //pass that text to the postData method on the parseService object which will then post it to the parse backend.
+  //The postData function will take whatever the user typed in (hint: look at the html and see what ng-model 
+  //correlates to on the input box), pass that text to the postData method on the parseService object which will 
+  //then post it to the parse backend.
 
 
 
 
   //uncomment this code when your getParseData function is finished
   //This goes and gets new data every second, which mimicking a chat room experience.
-  // setInterval(function(){
-  //   $scope.getParseData();
-  // }, 1500)
+
+  setInterval(function(){
+    $scope.getParseData();
+  }, 1500)
 })
--- js/parseService.js
+++ js/parseService.js
@@ -1,6 +1,26 @@
 var app = angular.module('chatroom');
 
-app.service('parseService', function($http){
+app.service('parseService', function($http, $q){
+
+
+  this.getData = function(){
+    return $http({
+      method: 'GET',
+      url: 'https://api.parse.com/1/classes/chat?order=-createdAt'
+    })
+  };
+
+  this.postData = function(text){
+    return $http({
+      method: 'POST',
+      data: text,
+      url: 'https://api.parse.com/1/classes/chat'
+    })
+  };
+
+});
+
+
   //Here you'll need to create two methods. One called postData and the other called getData.
   
   //On the lines below create a getData method. This method will retrieve data from the parse backend.
@@ -12,11 +32,37 @@ app.service('parseService', function($http){
   //On the line below create the postData method. This method will add data to the parse backend.
   //The url for the request needs to be 'https://api.parse.com/1/classes/chat'
   //Because we're making a POST request, we need a way to tell parse the data we want to give it, 
-    //in your $http call (along with url and method) have a data property which has a value that is equal to another object which a key of text and a value of the message being passed to parse. IE data: {text: yourMessage} 
-  //Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), you can then use .then in your controller.
+    //in your $http call (along with url and method) have a data property which has a value that is equal 
+    //to another object which a key of text and a value of the message being passed to parse. IE data: {text: yourMessage} 
+  //Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), 
+  //you can then use .then in your controller.
   
   //postData method here
   
   
   //getData method here
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
